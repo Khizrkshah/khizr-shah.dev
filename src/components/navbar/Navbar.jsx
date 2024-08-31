43b5e08e8172a91ba4addc39326c2666e05502c6
Mian Khizr Shah <khizrkshah@gmail.com>
2024-08-31 22:08:35 +0200

    Fixed animation in Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,18 +8,24 @@ const Navbar = () => {
       <div className="wrapper">
         <motion.span
           initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
         >
           Khizr Shah
         </motion.span>
-        <div className="social">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="social"
+        >
           <a href="#">
             <img src="/LinkedIn.png" alt="" />
           </a>
           <a href="#">
             <img src="/GitHub.png" alt="" />
           </a>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
