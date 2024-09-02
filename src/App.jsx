76491d9c2bc7688ff17b04a135da7bf6ce69921d
Mian Khizr Shah <khizrkshah@gmail.com>
2024-09-02 07:33:58 +0200

    More work on the Sidebar component, Added a Particle effect for the home screen using tsparticles

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import { useState } from "react";
 import "./App.scss";
 import Navbar from "./components/Navbar/Navbar";
+import ParticlesHomepage from "./components/particles/ParticlesHomepage";
 
 function App() {
   return (
     <div>
-      <section>
+      <section id="Homepage">
         <Navbar />
+        <div className="particleContainer">
+          <img className="hero" src="./hero.png"></img>
+          <ParticlesHomepage />
+        </div>
       </section>
-      <section>Parallax</section>
+      <section id="Services">Parallax</section>
       <section>Services</section>
+      <section id="Projects">Parallax</section>
       <section>Project1</section>
       <section>Project2</section>
       <section>Project3</section>
       <section>Project4</section>
       <section>Project5</section>
-      <section>Contact</section>
+      <section id="Contact">Contact</section>
     </div>
   );
 }
